6200ca4b25bdffed4112f093f2feb2b2f0562149
teddy1016 <yagang1016@outlook.com>
2023-07-17 21:35:52 +0800

    paginator added

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,10 +2,24 @@ from django.shortcuts import render,get_object_or_404
 from django.views import generic
 from .models import Post
 from .forms import CommentForm
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
-class PostList(generic.ListView):
-    queryset = Post.objects.filter(status=1).order_by('-created_on')
-    template_name = 'index.html'
+def PostList(request):
+    object_list = Post.objects.filter(status=1).order_by('-created_on')
+    paginator = Paginator(object_list, 1)  # 3 posts in each page
+    page = request.GET.get('page')
+    try:
+        post_list = paginator.page(page)
+    except PageNotAnInteger:
+            # If page is not an integer deliver the first page
+        post_list = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        post_list = paginator.page(paginator.num_pages)
+    return render(request,
+                  'index.html',
+                  {'page': page,
+                   'post_list': post_list})
 
 def post_detail(request, slug):
     template_name = 'post_detail.html'
